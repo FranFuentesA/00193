ba1f1ea63651e786e5bdd852e759e071ff46d5b2
Francisco Fuentes <fran_fuentes20@hotmail.com>
2016-01-22 11:29:53 +0100

    Comit primero

---
+++ Test.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Test here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Test
+{
+    // instance variables - replace the example below with your own
+
+    /**
+     * Constructor for objects of class Test vacio
+     */
+    public Test()
+    {
+    }
+
+    
+}
